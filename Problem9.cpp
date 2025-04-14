c5a24245339584056d711183d397d04c2f110b64
Vaibhav Modi <modivaibhav817@gmail.com>
2025-04-15 01:36:33 +0530

    some more

---
+++ Problem9.cpp
@@ -0,0 +1,43 @@
+// Calculate the sum of numbers in an array of numbers
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void predefinedArray() {
+    int predefinedArray[5] = {1, 2, 3, 4, 5};
+
+    int sum = 0;
+    for (int i = 0; i < 5; i++) {
+        sum = sum + predefinedArray[i];
+    }
+    cout << "sum of array is = " << sum << endl;
+}
+
+void dynamicArray() {
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    vector<int> numbers(n);
+
+    cout << "Enter " << n << " numbers:" << endl;
+    for(int i = 0; i < n; i++) {
+        cin >> numbers[i];
+    }
+
+    int sum = 0;
+    for (int i = 0; i < 5; i++) {
+        sum = sum + numbers[i];
+    }
+    cout << "sum of array is = " << sum << endl;
+    
+}
+
+
+
+int main() {
+    // predefinedArray();
+    dynamicArray();
+    return 0;
+}
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,1 @@
+cout << "sum of array is = " << sum << endl;
