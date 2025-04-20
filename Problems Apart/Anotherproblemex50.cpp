cca9efb1a6b8b84246717b16918da45dcb591c19
Vaibhav Modi <modivaibhav817@gmail.com>
2025-04-20 23:40:57 +0530

    moving&all

---
+++ Problems Apart/Anotherproblemex50.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int num, reverse, rem;
+    cout << "Enter a number to be reversed = ";
+    cin >> num;
+    reverse = 0;
+
+        while (num != 0) {
+            rem = num % 10;
+            reverse = reverse * 10 + rem;     
+            num /= 10;                        
+        }
+        cout << "The Reversed Number is = " << reverse << endl;
+    return 0;
+}
