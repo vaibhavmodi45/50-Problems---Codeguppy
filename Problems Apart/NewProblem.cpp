71016fb126ff8fce6deeecfcf0f1251b3e7a350b
Vaibhav Modi <modivaibhav817@gmail.com>
2025-06-07 23:55:29 +0530

    Commenting out some statements

--- Problems Apart/NewProblem.cpp
+++ Problems Apart/NewProblem.cpp
@@ -1,2 +1,3 @@
 // COntent a Problem
 // This file we gonna add up a problem that is listed in one of the 50 codeguppy coding problems
+// This file we gonna add up a problem that is listed in one of the 50 codeguppy coding problems 01
