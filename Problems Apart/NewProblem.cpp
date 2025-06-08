45f3608542764180542841d2709c967ff7828f62
Vaibhav Modi <modivaibhav817@gmail.com>
2025-06-08 20:02:27 +0530

    Commenting out some statements and making the file more organised

--- Problems Apart/NewProblem.cpp
+++ Problems Apart/NewProblem.cpp
@@ -1,3 +1,9 @@
 // COntent a Problem
 // This file we gonna add up a problem that is listed in one of the 50 codeguppy coding problems
 // This file we gonna add up a problem that is listed in one of the 50 codeguppy coding problems 01
+// COntent a Problem
+// This file we gonna add up a problem that is listed in one of the 50 codeguppy coding problems
+// This file we gonna add up a problem that is listed in one of the 50 codeguppy coding problems 01
+// COntent a Problem
+// This file we gonna add up a problem that is listed in one of the 50 codeguppy coding problems
+// This file we gonna add up a problem that is listed in one of the 50 codeguppy coding problems 01
